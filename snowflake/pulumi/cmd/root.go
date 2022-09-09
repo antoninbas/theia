eeeacd8091a36bb59c61c04b526ee91ad3f4c30f
Antonin Bas <abas@vmware.com>
2022-09-08 18:34:44 -0700

    Rename CLI from pulumi to theia-sf

--- snowflake/pulumi/cmd/root.go
+++ snowflake/pulumi/cmd/root.go
@@ -20,7 +20,7 @@ var logger logr.Logger
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "pulumi",
+	Use:   "theia-sf",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
